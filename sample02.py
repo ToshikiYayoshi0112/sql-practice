6c2136166e7c10e72ec5166df78d868cbb49e511
Yayoshi <sagauyayoshi@gmail.com>
2019-06-28 21:36:23 +0900

    PythonでsqlファイルにデータINSERT INTOで追加した。

---
+++ sample02.py
@@ -0,0 +1,22 @@
+"""
+目標:users.sqliteにINSERT文を発行する
+"""
+
+import sqlite3
+
+
+def main():
+    conn = sqlite3.connect("users.sqlite")
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO users (name, age) VALUES ('Bob', 15)"
+
+    cursor.execute(sql)
+
+    conn.commit()
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
